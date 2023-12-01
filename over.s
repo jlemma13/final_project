881d5ef4c2cd2f7c6097655ded4c8e4a5e01ff48
Justin Lemma <jlemma@mail.umw.edu>
2023-11-30 22:32:25 -0500

    Fixing over.s file

--- over.s
+++ over.s
@@ -7,12 +7,7 @@ over:
     mov r2, #0
     b .none
 .end:
-<<<<<<< HEAD
-    @bl game_over
-    bl reset
-=======
     mov r2, #1
->>>>>>> 4e3d43086952405864d55daa7d1a34731febf49c
 .none:
     mov r0, r2
     mov pc, lr
